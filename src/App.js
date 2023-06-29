6eb0641bfb519eef4fd9576686447e5b6f8a5e0a
Shir ben yosef <benyosefshir@gmail.com>
2023-06-29 13:33:52 +0300

    changed BattleStatsComponentMoreData to FairFightData and added to it battlestatscore

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StockApp from './components/StockApp';
 import BattleStatsComponentManual from './components/BattleStatsComponentManual';
 import BattleStatsComponent from './components/BattleStatsComponent';
-import BattleStatsComponentMoreData from './components/BattleStatsComponentMoreData';
+import FairFightData from './components/FairFightData';
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/stock-app" element={<StockApp />} />
         <Route path="/" element={<BattleStatsComponentManual />} />
         <Route path="/automat" element={<BattleStatsComponent />} />
-        <Route path="/automatMore" element={<BattleStatsComponentMoreData />} />
+        <Route path="/automatMore" element={<FairFightData />} />
         {/* More <Route> components go here for other paths */}
       </Routes>
     </Router>
