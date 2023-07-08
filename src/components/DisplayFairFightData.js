9ef81315e87aebbd452c718c0d80f1a008cc329b
Shir ben yosef <benyosefshir@gmail.com>
2023-07-08 18:24:51 +0300

    edited DisplayFairFightData.js to update the data into the supabase

--- src/components/DisplayFairFightData.js
+++ src/components/DisplayFairFightData.js
@@ -1,9 +1,11 @@
-// DisplayFairFightData.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useFetchFairFightData from '../hooks/useFetchFairFightData';
+import { supabase } from '../supabaseClient';
+import { useUserId } from '../hooks/useUserId';
 
 const DisplayFairFightData = () => {
   const [isKeySubmitted, setIsKeySubmitted] = useState(false);
+  const userId = useUserId();
 
   const submitApiKey = event => {
     event.preventDefault();
@@ -11,6 +13,53 @@ const DisplayFairFightData = () => {
   };
 
   const fairFightsData = useFetchFairFightData(isKeySubmitted);
+
+  const storeDataInDatabase = async (data) => {
+    const defenders = Object.keys(data);
+
+    try {
+      for (const defender of defenders) {
+        let { fairFightMultiplier, battlestatScore } = data[defender];
+
+        if (typeof battlestatScore === 'number') {
+          const { data: existingDefender, error } = await supabase
+            .from('defender_stats')
+            .select()
+            .eq('defender', defender)
+            .single();
+
+          if (existingDefender) {
+            await supabase
+              .from('defender_stats')
+              .update({
+                fairfightmultiplier: fairFightMultiplier,
+                battlestatscore: battlestatScore,
+              })
+              .eq('defender', defender);
+          } else {
+            await supabase
+              .from('defender_stats')
+              .insert([
+                {
+                  defender,
+                  fairfightmultiplier: fairFightMultiplier,
+                  battlestatscore: battlestatScore,
+                  attacker_id: userId,
+                },
+              ]);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error inserting/updating data:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (isKeySubmitted && Object.keys(fairFightsData).length > 0) {
+      storeDataInDatabase(fairFightsData);
+    }
+  }, [fairFightsData, isKeySubmitted]);
 
   return (
     <div>
