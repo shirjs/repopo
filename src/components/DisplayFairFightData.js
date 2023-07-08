bcbe7a86d019a35c8dff551374c6f0d9786c6119
Shir ben yosef <benyosefshir@gmail.com>
2023-07-08 20:14:33 +0300

    reduced requests to the supabase server made by DisplayFairFightData.js

--- src/components/DisplayFairFightData.js
+++ src/components/DisplayFairFightData.js
@@ -15,40 +15,54 @@ const DisplayFairFightData = () => {
   const fairFightsData = useFetchFairFightData(isKeySubmitted);
 
   const storeDataInDatabase = async (data) => {
-    const defenders = Object.keys(data);
+    let { data: existingDefendersData, error } = await supabase
+      .from('defender_stats')
+      .select('*');
 
-    try {
-      for (const defender of defenders) {
-        let { fairFightMultiplier, battlestatScore } = data[defender];
+    if (error) {
+      console.error('Error fetching defender data:', error);
+      return;
+    }
 
-        if (typeof battlestatScore === 'number') {
-          const { data: existingDefender, error } = await supabase
-            .from('defender_stats')
-            .select()
-            .eq('defender', defender)
-            .single();
+    // Create an object for quick access to existing defender data
+    const existingDefenders = existingDefendersData.reduce((obj, item) => {
+      obj[item.defender] = item;
+      return obj;
+    }, {});
 
-          if (existingDefender) {
-            await supabase
-              .from('defender_stats')
-              .update({
-                fairfightmultiplier: fairFightMultiplier,
-                battlestatscore: battlestatScore,
-              })
-              .eq('defender', defender);
-          } else {
-            await supabase
-              .from('defender_stats')
-              .insert([
-                {
-                  defender,
-                  fairfightmultiplier: fairFightMultiplier,
-                  battlestatscore: battlestatScore,
-                  attacker_id: userId,
-                },
-              ]);
-          }
+    // Split new data into data to update and data to insert
+    const dataToUpdate = [];
+    const dataToInsert = [];
+
+    Object.keys(data).forEach(defender => {
+      let { fairFightMultiplier, battlestatScore } = data[defender];
+
+      if (typeof battlestatScore === 'number') {
+        if (existingDefenders[defender]) {
+          dataToUpdate.push({
+            ...existingDefenders[defender],
+            fairfightmultiplier: fairFightMultiplier,
+            battlestatscore: battlestatScore,
+          });
+        } else {
+          dataToInsert.push({
+            defender,
+            fairfightmultiplier: fairFightMultiplier,
+            battlestatscore: battlestatScore,
+            attacker_id: userId,
+          });
         }
+      }
+    });
+
+    // Now update and insert data with a single request for each operation
+    try {
+      if (dataToUpdate.length > 0) {
+        await supabase.from('defender_stats').upsert(dataToUpdate, { returning: 'minimal' });
+      }
+
+      if (dataToInsert.length > 0) {
+        await supabase.from('defender_stats').insert(dataToInsert, { returning: 'minimal' });
       }
     } catch (error) {
       console.error('Error inserting/updating data:', error);
