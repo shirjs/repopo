52d8bee5da524d8b3771ccd2d6fb9984852748b4
Shir ben yosef <benyosefshir@gmail.com>
2023-07-08 22:49:47 +0300

    fixed another bug that prevented the API from updating correctly on ApiKeyForm

--- src/components/ApiKeyForm.jsx
+++ src/components/ApiKeyForm.jsx
@@ -4,57 +4,45 @@ import { supabase } from '../supabaseClient';
 import { useUserId } from '../hooks/useUserId';
 
 const ApiKeyForm = () => {
+  const [apiKey, setApiKey] = useState('');
   const userId = useUserId();
-  const [newApiKey, setNewApiKey] = useState(''); // Maintain the apiKey inside the component state.
   
   useEffect(() => {
-    // Fetch the existing API key when the userId changes
-    const fetchApiKey = async () => {
-      if (userId) {
-        const { data, error } = await supabase
-          .from('UserApiKey')
-          .select('api_key')
-          .eq('user_id', userId)
-          .single();
-
-        if (error) {
-          console.error('Error fetching API key:', error.message);
-        } else {
-          setNewApiKey(data?.api_key || '');
-        }
-      }
-    };
-
     fetchApiKey();
   }, [userId]);
+
+  const fetchApiKey = async () => {
+    if (userId) {
+      const { data } = await supabase
+        .from('UserApiKey')
+        .select('api_key')
+        .eq('user_id', userId)
+        .single();
+          
+      if (data) {
+        setApiKey(data.api_key);
+      }
+    }
+  };
 
   const handleSubmit = async event => {
     event.preventDefault();
 
-    if (userId === null) {
+    if (!userId) {
       console.log("User is not authenticated.");
       return;
     }
 
-    // Try to update the existing row first
-    let { count, error } = await supabase
+    const { data, error } = await supabase
       .from('UserApiKey')
-      .update({ api_key: newApiKey })
-      .eq('user_id', userId);
+      .upsert([
+        { user_id: userId, api_key: apiKey },
+      ], { returning: 'minimal' }); // 'minimal' will not return any data after insert/update
 
-    // If no rows were updated (i.e., the row does not exist), then insert a new one
-    if (count === 0) {
-      const { error } = await supabase
-        .from('UserApiKey')
-        .insert([
-          { user_id: userId, api_key: newApiKey },
-        ]);
-
-      if (error) {
-        console.log("Error inserting API Key: ", error.message);
-      }
-    } else if (error) {
-      console.log("Error updating API Key: ", error.message);
+    if (error) {
+      console.log("Error updating/inserting API Key: ", error.message);
+    } else if (data) {
+      setApiKey(data.api_key);
     }
   };
 
@@ -62,7 +50,7 @@ const ApiKeyForm = () => {
     <form onSubmit={handleSubmit}>
       <label>
         API Key:
-        <input type="text" value={newApiKey} onChange={(e) => setNewApiKey(e.target.value)} />
+        <input type="text" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
       </label>
       <input type="submit" value="Save API Key" />
     </form>
